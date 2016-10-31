e7a0534656e187cf07296f32bda163944dc9ad76
baudeveloper <webmaster@baustein.ca>
2016-10-31 15:59:34 -0400

    Updated Call-to-action.php file.

--- template-parts/call-to-action.php
+++ template-parts/call-to-action.php
@@ -5,18 +5,18 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12">
-				<?php if ( is_post_type_archive('financial-news') ) { ?>
-					<h1><?php post_type_archive_title(); ?></h1>
+				<?php if ( is_post_type_archive('financial-news') || is_singular( 'financial-news' ) ) { ?>
+					<h1><?php esc_html_e( 'Financial News', 'nobelium' ); ?></h1>
 				<?php } else if ( in_category( 'news' ) ) { ?>
 					<h1><?php esc_html_e( 'News', 'nobelium' ); ?></h1>
 				<?php } else if ( is_404() ) { ?>
 					<h1><?php esc_html_e( 'Sorry', 'nobelium' ); ?></h1>
 				<?php } else if ( is_search() ) { ?>
 					<h1><?php esc_html_e( 'Search', 'nobelium' ); ?></h1>
-				<?php } else if ( is_post_type_archive('shareholder-reports') ) { ?>
-					<h1><?php post_type_archive_title(); ?></h1>
-				<?php } else if ( is_post_type_archive('management-ppts') ) { ?>
-					<h1><?php post_type_archive_title(); ?></h1>
+				<?php } else if ( is_post_type_archive('shareholder-reports') || is_singular( 'shareholder-reports' ) ) { ?>
+					<h1><?php esc_html_e( 'Shareholder Reports', 'nobelium' ); ?></h1>
+				<?php } else if ( is_post_type_archive('management-ppts') || is_singular( 'management-ppts' ) ) { ?>
+					<h1><?php esc_html_e( 'Management Presentations', 'nobelium' ); ?></h1>
 				<?php } else { ?>
 				<h1><?php the_title(); ?></h1>
 				<?php } ?>
